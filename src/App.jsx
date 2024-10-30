0c25eae08bb0be1b077c1fccf931c1bcf353108f
Mark <hendriksen-mark@hotmail.com>
2024-10-30 23:37:50 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,6 @@ const loading = (
 const App = () => {
   const [API_KEY, setAPI_KEY] = useState();
 
-  //const HOST_IP = "http://localhost"; // Pass the IP (http://x.x.x.x) of the diyHue Bridge, if running through npm start
-  //const HOST_IP = "http://192.168.1.25"; // Pass the IP (http://x.x.x.x) of the diyHue Bridge, if running through npm start
-  //const HOST_IP = "http://192.168.1.11"; // Pass the IP (http://x.x.x.x) of the diyHue Bridge, if running through npm start
-  //const HOST_IP = "http://192.168.1.3"; // Pass the IP (http://x.x.x.x) of the diyHue Bridge, if running through npm start
   const HOST_IP = ""; // Pass the IP (http://x.x.x.x) of the diyHue Bridge, if running through npm start
 
   useEffect(() => {
